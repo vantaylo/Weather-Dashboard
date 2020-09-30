d8e5bbd9f2130d4f8ca8ce0f095761dcbcd4dd1d
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-30 13:34:06 -0500

    Call for UVIndex, Function for displaying 5 day forecast

--- weather.js
+++ weather.js
@@ -56,3 +56,48 @@ function currentWeather(city) {
     }
   });
 }
+
+function UVIndex(ln, lt) {
+  var uvqURL =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+    APIKey +
+    "&lat=" +
+    lt +
+    "&lon=" +
+    ln;
+  $.ajax({
+    url: uvqURL,
+    method: "GET",
+  }).then(function (response) {
+    $(currentUvindex).html(response.value);
+  });
+}
+
+function forecast(cityid) {
+  var dayover = false;
+  var queryforcastURL =
+    "https://api.openweathermap.org/data/2.5/forecast?id=" +
+    cityid +
+    "&appid=" +
+    APIKey;
+  $.ajax({
+    url: queryforcastURL,
+    method: "GET",
+  }).then(function (response) {
+    for (i = 0; i < 5; i++) {
+      var date = new Date(
+        response.list[(i + 1) * 8 - 1].dt * 1000
+      ).toLocaleDateString();
+      var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
+      var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+      var tempK = response.list[(i + 1) * 8 - 1].main.temp;
+      var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(2);
+      var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
+
+      $("#fDate" + i).html(date);
+      $("#fImg" + i).html("<img src=" + iconurl + ">");
+      $("#fTemp" + i).html(tempF + "&#8457");
+      $("#fHumidity" + i).html(humidity + "%");
+    }
+  });
+}
